a8cea0e07cf4cd5671a5bb14df90ef2f604024cf
phu <phutq@htactive.com>
2020-10-06 14:55:52 +0700

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,112 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-container>
+      <v-card class="mx-auto" max-width="500">
+        <v-toolbar color="deep-purple accent-4" dark>
+          <v-toolbar-title>List Demo</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+
+        <v-list subheader>
+          <v-subheader>Render List</v-subheader>
+          <v-list-item>
+            <v-list-item-content>
+              <v-text-field label="Value add" v-model="valueAdd"></v-text-field>
+            </v-list-item-content>
+            <v-list-item-icon>
+              <v-btn color="primary" @click="addList">Add</v-btn>
+            </v-list-item-icon>
+          </v-list-item>
+          <v-list-item v-for="item in lists" :key="item.title">
+            <v-list-item-avatar>
+              <v-img :alt="`${item.title} avatar`" :src="item.avatar"></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-text-field
+                :value="item.title"
+                @change="updateValue(item.id, $event)"
+              ></v-text-field>
+            </v-list-item-content>
+
+            <v-list-item-icon>
+              <v-icon color="red" @click="deleteItem(item.id)">
+                mdi-delete
+              </v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-container>
+  </v-app>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  data: () => ({
+    valueAdd: "",
+    lists: [],
+    dataAPI: [
+      {
+        id: 1,
+        active: true,
+        avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
+        title: "Jason Oner",
+      },
+      {
+        id: 2,
+        active: true,
+        avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+        title: "Mike Carlson",
+      },
+      {
+        id: 3,
+        avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+        title: "Cindy Baker",
+      },
+      {
+        id: 4,
+        avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
+        title: "Ali Connors",
+      },
+    ],
+  }),
+  methods: {
+    getData() {
+      //Call API get list
+      this.lists = this.dataAPI;
+    },
+    deleteItem(id) {
+      // API thì call API delete rồi gọi hàm get Data
+      this.lists = this.lists.filter((item) => item.id !== id);
+    },
+    updateValue(id, value) {
+      this.lists = this.lists.map((item) => {
+        if (item.id == id) {
+          return { ...item, title: value };
+        }
+        return item;
+      });
+    },
+    addList() {
+      if (this.valueAdd) {
+        // call API create
+        this.lists = [
+          ...this.lists,
+          {
+            id: Math.ceil(Math.random(1000) * 100),
+            avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+            title: this.valueAdd,
+          },
+        ];
+        this.valueAdd = "";
+      }
+    },
+  },
+  mounted() {
+    // Load data
+    // Call api get list
+    this.getData();
+    console.log("load Data success");
+  },
+};
+</script>
---
+++ src/views/RegionList.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-data-table :headers="headers" :items="desserts" class="elevation-1" hide-default-footer>
+    <template v-slot:item.country="{ item }">{{ showCountry(item.country)}}</template>
+    <template v-slot:item.action="{ }">
+      <v-btn class="mx-2" fab dark small color="cyan">
+        <v-icon dark>mdi-pencil</v-icon>
+      </v-btn>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: "RegionList",
+  data() {
+    return {
+      listCountry: [
+        { id: 1, name: "Viet Nam" },
+        { id: 2, name: "My" },
+        { id: 3, name: "Phap" },
+        { id: 4, name: "Anh" },
+      ],
+      headers: [
+        {
+          text: "Name",
+          align: "start",
+          sortable: false,
+          value: "name",
+        },
+        { text: "Description", value: "desc", sortable: false },
+        { text: "Country", value: "country", sortable: false },
+        { text: "Boundary", value: "boundary", sortable: false },
+        {
+          text: "Centerpoint Longitude",
+          value: "c_longitude",
+          sortable: false,
+        },
+        { text: "Centerpoint Latitude", value: "c_latitude", sortable: false },
+        { text: "Header Image", value: "header_image", sortable: false },
+        { text: "", value: "action", sortable: false },
+      ],
+      desserts: [
+        {
+          name: "Frozen Yogurt",
+          desc: "đasadasdsadsadasd",
+          country: 1,
+          boundary: "đâsdsadas",
+          c_longitude: "44.312312",
+          c_latitude: "-17.3123",
+          header_image: "image/abc",
+        },
+        {
+          name: "Frozen Yogurt1",
+          desc: "đasadasdsadsadasd",
+          country: 2,
+          boundary: "đâsdsadas",
+          c_longitude: "44.312312",
+          c_latitude: "-17.3123",
+          header_image: "image/abc",
+        },
+        {
+          name: "Frozen Yogurt2",
+          desc: "đasadasdsadsadasd",
+          country: 3,
+          boundary: "đâsdsadas",
+          c_longitude: "44.312312",
+          c_latitude: "-17.3123",
+          header_image: "image/abc",
+        },
+        {
+          name: "Frozen Yogurt3",
+          desc: "đasadasdsadsadasd",
+          country: 4,
+          boundary: "đâsdsadas",
+          c_longitude: "44.312312",
+          c_latitude: "-17.3123",
+          header_image: "image/abc",
+        },
+      ],
+    };
+  },
+  methods: {
+    showCountry(id) {
+      return this.listCountry.find((item) => item.id === id).name;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
